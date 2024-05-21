01c9c1d4d99ffe55408ffe9c93d8b9822ad4f5f1
ohyujeong <ohlife4@gmail.com>
2024-05-21 16:30:05 +0900

    [Fix/Feat] user가 입출금 거래내역을 모두 조회할 수 있도록 수정, 최근 송금계좌번호 목록 조회 API

--- src/main/java/com/flab/tess/controller/TransactionController.java
+++ src/main/java/com/flab/tess/controller/TransactionController.java
@@ -1,7 +1,9 @@
 package com.flab.tess.controller;
 
 
+import com.flab.tess.domain.TransactionType;
 import com.flab.tess.domain.Transaction;
+import com.flab.tess.domain.User;
 import com.flab.tess.dto.*;
 import com.flab.tess.service.CustomUserDetailService;
 import com.flab.tess.service.TransactionService;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -37,30 +42,68 @@ public class TransactionController {
 
     //(2) 거래 내역 상세 조회
     @GetMapping("/{transactionId}")
-    public TransactionResponseDto getTransactionOne(Principal principal, @PathVariable("transactionId") String id){
-        UserDto user = customUserDetailService.findUser(principal);
+    public TransactionResponse getTransactionOne(Principal principal, @PathVariable("transactionId") String id){
+        User user = customUserDetailService.findUser(principal);
         BigInteger transactionId = new BigInteger(id);
         Transaction transaction = transactionService.getTransaction(transactionId);
-        return TransactionResponseDto.from(transaction);
+        //+ 내역인지(user에게 입금된 내역=> user가 receiver인 거래 내역)
+        if(transaction.getReceiverAccountId().getUser().equals(user)){
+            return TransactionResponse.from(transaction, transaction.getReceiverAccountId(), transaction.getReceiverBalance(), TransactionType.WITHDRAW, transaction.getSenderAccountId());
+        }
+        //- 내역인지(user로부터 출금된 내역=> user가 sender인 거래 내역)
+        else{
+            return TransactionResponse.from(transaction, transaction.getSenderAccountId(), transaction.getSenderBalance(), TransactionType.DEPOSIT, transaction.getReceiverAccountId());
+        }
     }
 
     //(3) 거래 내역 계좌별 전체 조회 GET
     @GetMapping("/accounts/{accountId}")
-    public List<TransactionResponseDto> getTransactionAll(@PathVariable("accountId") String id){
+    public List<TransactionAllResponse> getTransactionAll(@PathVariable("accountId") String id){
         BigInteger accountId = new BigInteger(id);
-        List<Transaction> transactions = transactionService.getTransactionAll(accountId);
-        return transactions.stream() // 리스트를 스트림으로 변환
-                .map(TransactionResponseDto::from)
+
+        List<TransactionAllResponse> transactionAllResponses = new ArrayList<>();
+        //+ 내역인지(user에게 입금된 내역=> user가 receiver인 거래 내역)
+        List<Transaction> transactionsWithdraw = transactionService.getTransactionWithdraw(accountId);
+        //- 내역인지(user로부터 출금된 내역=> user가 sender인 거래 내역)
+        List<Transaction> transactionsDeposit = transactionService.getTransactionDeposit(accountId);
+
+        // Withdraw transactions
+        transactionAllResponses.addAll(
+                transactionsWithdraw.stream()
+                        .map(transaction -> { return TransactionAllResponse.from(transaction, TransactionType.WITHDRAW, transaction.getReceiverBalance(), transaction.getSenderAccountId());
+                        })
+                        .collect(Collectors.toList())
+        );
+
+        // Deposit transactions
+        transactionAllResponses.addAll(
+                transactionsDeposit.stream()
+                        .map(transaction -> { return TransactionAllResponse.from(transaction, TransactionType.DEPOSIT, transaction.getSenderBalance(), transaction.getReceiverAccountId());
+                        })
+                        .collect(Collectors.toList())
+        );
+
+        // Sort by transactionAt
+        return transactionAllResponses.stream()
+                .sorted(Comparator.comparing(TransactionAllResponse::getTransactionAt))
                 .collect(Collectors.toList());
     }
 
-//    //(4) 송금 할 때 최근 보낸 계좌 조회
-//    @GetMapping(value = "/{accountId}")
-//    public ResponseEntity<List<RecentWithdrawDto>> getRecentWithdraw(@PathVariable("accountId") String id){
-//        BigInteger accountId = new BigInteger(id);
-//        List<RecentWithdrawDto> recentWithdrawDto = transactionService.
-//        return ResponseEntity.status(HttpStatus.OK).body(recentWithdrawDto);
-//    }
+    //(4) user가 최근 송금한 계좌 조회
+    @GetMapping(value = "withdraw/{accountId}")
+    public List<RecentWithdrawResponse> getRecentWithdraw(@PathVariable("accountId") String id){
+        BigInteger accountId = new BigInteger(id);
+        // user가 sender인 내역
+        List<Transaction> transactions = transactionService.getTransactionDeposit(accountId);
+
+        // 중복된 accountNum을 제외하고 최대 5개의 결과 반환
+        Set<String> seenAccountNum = new java.util.HashSet<>();
+        return transactions.stream()
+                .filter(transaction -> seenAccountNum.add(transaction.getReceiverAccountId().getAccountNum()))
+                .limit(5)
+                .map(RecentWithdrawResponse::from)
+                .collect(Collectors.toList());
+    }
 
 
 }
--- src/main/java/com/flab/tess/dto/OtherAccountDto.java
+++ src/main/java/com/flab/tess/dto/OtherAccountDto.java
@@ -10,13 +10,14 @@ import java.math.BigInteger;
 @Getter
 @Builder
 @RequiredArgsConstructor
-public class ReceiveAccountDto {
+//거래를 하는 상대방의 계좌 정보니까 계좌번호만 노출
+public class OtherAccountDto {
 
     private final BigInteger accountId;
     private final String accountNum;
 
-    public static ReceiveAccountDto from(Account account){
-        return ReceiveAccountDto.builder()
+    public static OtherAccountDto from(Account account){
+        return OtherAccountDto.builder()
                 .accountId(account.getAccountId())
                 .accountNum(account.getAccountNum())
                 .build();
