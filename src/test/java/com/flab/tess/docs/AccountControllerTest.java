79e5ed1e1df07d377769f26b75c1b20fa24b454a
ohyujeong <ohlife4@gmail.com>
2024-06-15 19:12:18 +0900

    [Test] 계좌 생성 api test코드 작성

--- src/test/java/com/flab/tess/docs/AccountControllerTest.java
+++ src/test/java/com/flab/tess/docs/AccountControllerTest.java
@@ -3,27 +3,27 @@ package com.flab.tess.docs;
 import com.flab.tess.controller.AccountController;
 import com.flab.tess.domain.Account;
 import com.flab.tess.domain.User;
+import com.flab.tess.dto.AccountCreateRequest;
+import com.flab.tess.dto.AccountResponse;
+import com.flab.tess.dto.WithdrawalRequestDto;
 import com.flab.tess.service.AccountService;
 import com.flab.tess.service.CustomUserDetailService;
-import com.flab.tess.service.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import org.mockito.MockitoAnnotations;
+import org.mockito.ArgumentMatchers;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
-
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.security.Principal;
 import java.util.List;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -96,6 +96,37 @@ public class AccountControllerTest extends RestDocsTest {
                                 fieldWithPath("accountName").description("계좌 이름").type(JsonFieldType.STRING).optional(),
                                 fieldWithPath("accountType").description("계좌 타입").type(JsonFieldType.STRING).optional(),
                                 fieldWithPath("balance").description("잔액").type(JsonFieldType.STRING).optional()
+                        )));
+    }
+
+    @Test
+    void 계좌_생성() throws Exception{
+
+        //given
+        AccountCreateRequest testAccountCreateRequest = new AccountCreateRequest("123", "test계좌", "입출금");
+        Account account = Account.of(testUser, "123", "test계좌", "입출금");
+
+        given(customUserDetailService.findUser(any(Principal.class))).willReturn(testUser);
+        given(accountService.saveAccount(any(AccountCreateRequest.class), any(User.class))).willReturn(account);
+
+
+        // When & Then
+        mockMvc.perform(post("/accounts")
+                .principal(mockPrincipal)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(testAccountCreateRequest)))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                // 문서화
+                .andDo(document("create-account",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("accountId").description("계좌 id"),
+                                fieldWithPath("accountNum").description("계좌 번호"),
+                                fieldWithPath("accountName").description("계좌 이름"),
+                                fieldWithPath("accountType").description("계좌 유형"),
+                                fieldWithPath("balance").description("잔액")
                         )));
     }
 
