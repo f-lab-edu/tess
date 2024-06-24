1adea13803e1d6fa3c122c2f18b55ff05d624fa0
ohyujeong <ohlife4@gmail.com>
2024-06-24 20:53:26 +0900

    [Feat] 유저 정보 조회에 cache 적용

--- src/main/java/com/flab/tess/dto/UserDto.java
+++ src/main/java/com/flab/tess/dto/UserDto.java
@@ -1,24 +1,16 @@
 package com.flab.tess.dto;
 
-import com.flab.tess.domain.User;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.math.BigInteger;
 
 @Getter
-@Builder
 @RequiredArgsConstructor
 public class UserDto {
 
-    private final BigInteger id;
+    private final BigInteger userId;
+    private final String loginId;
     private final String userName;
 
-    public static UserDto from(User user){
-        return UserDto.builder()
-                .id(user.getUserId())
-                .userName(user.getName())
-                .build();
-    }
 }
--- src/test/java/com/flab/tess/docs/AuthControllerTest.java
+++ src/test/java/com/flab/tess/docs/AuthControllerTest.java
@@ -6,30 +6,43 @@ import com.flab.tess.dto.JoinRequest;
 import com.flab.tess.dto.LoginRequest;
 import com.flab.tess.dto.UserDto;
 import com.flab.tess.provider.JwtTokenProvider;
+import com.flab.tess.service.CustomUserDetailService;
 import com.flab.tess.service.UserService;
 import com.mysql.cj.log.Log;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.http.MediaType;
 
+import java.math.BigInteger;
+import java.security.Principal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AuthControllerTest extends RestDocsTest{
 
     private final UserService userService = mock(UserService.class);
     private final JwtTokenProvider jwtTokenProvider = mock(JwtTokenProvider.class);
-
+    private final CustomUserDetailService customUserDetailService = mock(CustomUserDetailService.class);
 
     @Override
     protected Object initializeController() {
-        return new AuthController(userService, jwtTokenProvider);
+        return new AuthController(userService, jwtTokenProvider, customUserDetailService);
     }
-
 
     @Test
     void 회원가입() throws Exception{
@@ -47,10 +60,36 @@ public class AuthControllerTest extends RestDocsTest{
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
                         responseFields(
-                                fieldWithPath("id").description("유저 id").optional(),
+                                fieldWithPath("userId").description("유저 id").optional(),
+                                fieldWithPath("loginId").description("유저 loginId"),
                                 fieldWithPath("userName").description("사용자 이름")
                         )
                 ));
+    }
+
+    @Test
+    void 유저_정보_조회() throws Exception{
+
+        given(customUserDetailService.findUser(mockPrincipal)).willReturn(testUser);
+        given(userService.getUserInfo(testUser.getUserId())).willReturn(testUser);
+
+        //when & then
+        this.mockMvc.perform(
+                get("/auth/info").principal(mockPrincipal))
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith("application/json"))
+                .andExpect(status().isOk())
+        //문서화
+                .andDo(document("info",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("userId").description("유저 id").optional(),
+                                fieldWithPath("loginId").description("유저 loginId"),
+                                fieldWithPath("userName").description("사용자 이름")
+                        )
+                ));
+
     }
 
     @Test
